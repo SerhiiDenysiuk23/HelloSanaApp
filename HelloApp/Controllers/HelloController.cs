b5f71b40f19d7a46630ef7cfbae4fc16f15c72a6
Сергій Денисюк <kn202_dsm@student.ztu.edu.ua>
2023-03-25 20:03:29 +0200

    Completed task

---
+++ HelloApp/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloApp.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Welcome(string name) { 
+            return View("Welcome", name); 
+        }
+    }
+}
